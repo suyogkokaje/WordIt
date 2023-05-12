612be8d422a980b8b201a5f1d512a730255b2f1e
suyogkokaje <suyogkokaje6@gmail.com>
2023-05-12 14:40:20 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin:"*"}));
 app.use("/posts", require("./routes/postRoutes"));
 app.use("/user", require("./routes/authRoute"));
 
